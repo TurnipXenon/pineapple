93a9c0b555bda94c14a72f3da5145638d56acc85
turnipxenon <turnipxenon@gmail.com>
2024-03-22 01:39:37 -0600

    Export Card

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,7 @@
 export { default as PineappleLayoutBase } from "./components/pineapple/PineappleBaseLayout.svelte";
 export { default as SeaweedTemplate } from "./template/SeaweedTemplate.svelte";
 export { default as LazyAsset } from "./components/LazyAsset.svelte";
+export { default as Card } from "./components/Card.svelte";
 
 export * from "./components/dialog_manager/DialogManagerStore";
 export * from "./components/dialog_manager/DialogManager";
