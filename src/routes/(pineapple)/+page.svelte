21b9b54a92b65392a7ac0e3ea4e0f0c87563325f
turnipxenon <turnipxenon@gmail.com>
2024-03-22 04:00:44 -0600

    Replace btn-group postcss class with turnip-menu
    There is postcss defined class called btn-group which has styles that have !important, which I accidentally used not knowing it exists, and it overrides whatever styles I apply on my class

--- src/routes/(pineapple)/+page.svelte
+++ src/routes/(pineapple)/+page.svelte
@@ -19,24 +19,23 @@
 	<div slot="content" class="content">
 		<h1 class="mb-8" style="font-weight: bolder">Directory</h1>
 
-		<div class="btn-group">
+		<div class="turnip-menu">
 			<button on:click={createGoToFunction("pineapple")}><h2>Pineapple playground</h2></button>
-			<button on:click={createGoToFunction("portfolio")}><h2>Seaweed playrground</h2></button>
+			<button on:click={createGoToFunction("portfolio")}><h2>Seaweed playground</h2></button>
 		</div>
 	</div>
 </Card>
 
 <style lang="postcss">
-    .btn-group {
+    .turnip-menu {
         padding: 2em;
-		    display: flex;
-		    gap: 2em;
-		    flex-direction: column;
-
+        display: flex;
+        gap: 2em;
+        flex-direction: column;
     }
 
     button {
-		    @apply btn variant-filled-secondary;
+        @apply btn variant-filled-secondary;
     }
 
     .content {
