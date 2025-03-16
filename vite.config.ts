3abdc4a837905f24a25ad0f208f692d10f32752f
turnipxenon <turnipxenon@gmail.com>
2025-03-15 20:27:50 -0600

    Add ParaglideJS

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
+import { paraglideVitePlugin } from "@inlang/paraglide-js";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
+		paraglideVitePlugin({
+			project: "./project.inlang",
+			outdir: "./src/lib/paraglide",
+			strategy: ["url", "cookie", "baseLocale"]
+		})
 	],
 	assetsInclude: ["**/*.yarn"]
 });
