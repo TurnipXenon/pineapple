fd65bc05b58574b27e529cc3329b4c43edc13b4a
turnipxenon <turnipxenon@gmail.com>
2025-03-24 21:00:20 -0600

    Fix circular dependency in BlogTemplate.svelte

--- src/lib/components/blog_template/BlogTemplate.svelte
+++ src/lib/components/blog_template/BlogTemplate.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import { Card, enableDialogueOverlay, type SimplePageMeta } from "$pkg";
 	import "./blog-template.css";
 	import { enableBackground } from "$pkg/store";
 	import { onDestroy, onMount } from "svelte";
 	import BlogTemplateInner from "$pkg/components/blog_template/BlogTemplateInner.svelte";
+	import type { SimplePageMeta } from "$pkg/components/navigation_component/index";
+	import { enableDialogueOverlay } from "$pkg/components/dialog_manager/DialogManagerStore";
+	import { Card } from "$pkg/components/index";
 
 	// grab page meta from the adjacent meta.json
 	interface Props {
