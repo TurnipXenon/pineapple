a6a83d99d24242cc2cb05b6c99a1fd091a2dd79e
turnipxenon <turnipxenon@gmail.com>
2025-04-19 19:09:17 -0600

    feat: add meta for parsnip pages

--- src/lib/modules/parsnip/route-util/ParsnipBlog.svelte
+++ src/lib/modules/parsnip/route-util/ParsnipBlog.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import type { PageMeta } from "$pkg";
+	import { OverridableMeta, type PageMeta } from "$pkg";
 	import ParsnipBlockChildren from "$pkg/modules/parsnip/ParsnipBlockChildren.svelte";
 	import type { ParsnipEntry } from "$pkg/modules/parsnip/ParsnipEntry";
 	import BlogTemplate from "$pkg/ui/templates/blog_template/BlogTemplate.svelte";
@@ -15,6 +15,13 @@
 		lastUpdated: new Date(parsnipEntry.stat.mtime).toLocaleString()
 	});
 </script>
+
+<OverridableMeta
+	title={parsnipEntry.basename}
+	ogTitle={parsnipEntry.basename}
+	ogDescription=""
+	ogImage={parsnipEntry.preview}
+/>
 
 <BlogTemplate pageMeta={pageMeta}>
 	<ParsnipBlockChildren blockChildren={parsnipEntry.ast.ast.children} />
