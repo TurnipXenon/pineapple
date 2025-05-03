3a402d2b3bc22c87e13e37fb6fc8a968b1ea1da1
turnipxenon <turnipxenon@gmail.com>
2025-05-02 21:15:12 -0600

    feat: add prerendered list of entries

--- src/routes/(pineapple)/pineapple/(pineapple)/[...slug]/+page.server.ts
+++ src/routes/(pineapple)/pineapple/(pineapple)/[...slug]/+page.server.ts
@@ -1,5 +1,29 @@
+import type { ParsnipOverall } from "$pkg/modules/parsnip/ParsnipOverall";
 import { slugPageServerLoad } from "$pkg/modules/parsnip/route-util/slugPageServerLoad";
-import type { PageServerLoad } from "./$types";
+import { getCmsBaseUrl } from "$pkg/util/env-getter";
+import type { EntryGenerator, PageServerLoad } from "./$types";
 
+// https://svelte.dev/docs/kit/page-options#entries
+const getEntries = async () => {
+	const baseUrl = getCmsBaseUrl();
+	const mainMeta = await fetch(`${baseUrl}/main.meta.json`);
+
+	if (!mainMeta.ok) {
+		console.error("Failed to get entries");
+		return [];
+	}
+
+	const parsnipOverall = {
+		...(await mainMeta.json()),
+		baseUrl
+	} as ParsnipOverall;
+
+	return parsnipOverall.files.map(f => ({
+		slug: f.slug
+	}));
+};
+
+
+export const entries: EntryGenerator = getEntries;
 export const prerender = true;
 export const load: PageServerLoad = slugPageServerLoad;
