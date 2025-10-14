a9e941040343bb81ad6d75e76dda56fd2f9a8bb3
turnipxenon <turnipxenon@gmail.com>
2025-10-14 01:18:52 -0400

    fix: accept clouflare environment variables

--- src/lib/util/env-getter.ts
+++ src/lib/util/env-getter.ts
@@ -1,2 +1,4 @@
-export const getCmsBaseUrl = () => import.meta.env.VITE_CMS_BASE_URL ?? 'https://gitlab.com/turnipxenon-personal/test-obdisian/-/raw/main';
-export const getWebBaseUrl= () => import.meta.env.VITE_WEB_BASE_URL ?? '/pineapple'
+// import.meta.env.* works default everywhere
+// env.* is for cloudflare
+export const getCmsBaseUrl = () => import.meta.env.VITE_CMS_BASE_URL ?? env?.VITE_CMS_BASE_URL ?? 'https://gitlab.com/turnipxenon-personal/test-obdisian/-/raw/main';
+export const getWebBaseUrl= () => import.meta.env.VITE_WEB_BASE_URL ?? env?.VITE_WEB_BASE_URL ?? '/pineapple'
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,8 +1,5 @@
 // from https://github.com/sveltejs/kit/issues/8882#issuecomment-1426906230
-declare module "$env/static/public" {
-	export const PUBLIC_CRINGE_USERNAME: <type>;
-	export const PUBLIC_TWEET1: <type>;
-	export const PUBLIC_TWEET2: <type>;
-	export const PUBLIC_TUMBLR: <type>;
-	export const PUBLIC_TWITTER_ID: <type>;
+declare namespace env {
+	export const VITE_CMS_BASE_URL: string;
+	export const VITE_WEB_BASE_URL: string;
 }
