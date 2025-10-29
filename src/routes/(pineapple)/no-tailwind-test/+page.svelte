9c0ab5485625a1b1ce6346fd8753ca19fa3a733c
turnipxenon <turnipxenon@gmail.com>
2025-10-28 23:28:47 -0400

    feat: proof of concept of tailwind alternative

---
+++ src/routes/(pineapple)/no-tailwind-test/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import TestButton from "./components/TestButton.svelte";
+</script>
+
+<TestButton class="test-button-scoped">
+</TestButton>
+
+<style>
+    :global {
+        .test-button-scoped {
+            display: flex;
+            flex-direction: row;
+		        gap: 2em;
+
+            div {
+                padding: 2rem;
+                background-color: gray;
+            }
+        }
+    }
+</style>
---
+++ src/routes/(pineapple)/no-tailwind-test/components/TestButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+	// import type { Snippet } from "svelte";
+
+	// interface Props {
+	// 	// children: Snippet;
+	// }
+
+	import type { HTMLAttributes } from "svelte/elements";
+
+	let _props: HTMLAttributes<HTMLDivElement> = $props();
+</script>
+<div {..._props}>
+	<div>1</div>
+	<div>2</div>
+</div>
