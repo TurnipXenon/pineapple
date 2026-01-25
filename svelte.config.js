b67ffa3b2451317f6ebe4fdfa4fafa3be0b5df69
turnipxenon <turnipxenon@gmail.com>
2026-01-25 02:38:44 -0500

    build: add sass

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			$pkg: path.resolve("./src/lib")
+			$pkg: path.resolve("./src/lib"),
+			$styles: path.resolve("./src/lib/styles"),
 		}
 	}
 };
