9f8e62a2655afb12b8eaefc555d45365ac83692e
turnipxenon <turnipxenon@gmail.com>
2026-01-31 16:57:26 -0500

    feat: add code block documentation page

--- src/routes/(pineapple)/documentation/components/getting-started/+page.svelte
+++ src/routes/(pineapple)/documentation/components/getting-started/+page.svelte
@@ -1,57 +1,26 @@
-<script lang="ts"></script>
+<script>
+	import { CodeBlock, TextLink } from "$pkg/ui/elements/index.ts";
+</script>
 
-<h1>Getting Started</h1>
-TODO: here
-TODO: here
+<h1>Entry Order Config</h1>
 
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
-<h2>Filler header</h2>
-YEah
+<h2>About</h2>
+<p>EntryOrderConfig renders the "Site ordering" controls used by the Seaweed layout advanced settings.
+	It lets you reorder groups, reorder entries within a group, and add/remove entries.
+	As you make changes it builds an `order=` query string that SeaweedLayout can parse to reproduce the same layout.</p>
 
-<style>
-</style>
+<h2>Sample</h2>
+
+<p><b>Not available yet</b></p>
+
+<h2>Usage</h2>
+
+<CodeBlock code={`<EntryOrderConfig
+  bind:layout={actualLayout}
+  bind:orderUrl={orderUrl}
+  allEntries={entryList}
+/>`} lang="markdown" classes="max-w-2xl" />
+
+<p>Source code:
+	<TextLink target="_blank" href="https://github.com/TurnipXenon/pineapple/blob/70a3193488d345e609b9151f27f17511650bb12f/src/lib/ui/templates/SeaweedLayout/SeaweedLayout.svelte#L240">https://github.com/TurnipXenon/pineapple/blob/70a3193488d345e609b9151f27f17511650bb12f/src/lib/ui/templates/SeaweedLayout/SeaweedLayout.svelte#L240</TextLink>
+</p>
---
+++ src/routes/(pineapple)/documentation/elements/code-block/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { CodeBlock, TextLink } from "$pkg/ui/elements/index.ts";
+</script>
+
+<h1>Code block</h1>
+
+<h2>About</h2>
+<p>For formatted code</p>
+
+<h2>Sample</h2>
+
+<CodeBlock code={`<EntryOrderConfig
+  bind:layout={actualLayout}
+  bind:orderUrl={orderUrl}
+  allEntries={entryList}
+/>`} lang="ts" classes="max-w-2xl" />
+
+
+<h2>Usage</h2>
+
+<CodeBlock code={`<CodeBlock code={\`<EntryOrderConfig
+  bind:layout={actualLayout}
+  bind:orderUrl={orderUrl}
+  allEntries={entryList}
+/>\`} lang="ts" classes="max-w-2xl" />`} lang="ts" classes="max-w-2xl" />
+
+<p>Source code:
+	<TextLink target="_blank" href="https://github.com/TurnipXenon/pineapple/blob/2802a54370bd643004f0b4d5d40431b476e1b5f2/src/lib/ui/elements/CodeBlock/CodeBlock.svelte">https://github.com/TurnipXenon/pineapple/blob/2802a54370bd643004f0b4d5d40431b476e1b5f2/src/lib/ui/elements/CodeBlock/CodeBlock.svelte</TextLink>
+</p>
+<p>Example code:
+	<TextLink target="_blank" href="https://github.com/TurnipXenon/pineapple/blob/70a3193488d345e609b9151f27f17511650bb12f/src/lib/ui/templates/SeaweedLayout/SeaweedLayout.svelte#L248">https://github.com/TurnipXenon/pineapple/blob/70a3193488d345e609b9151f27f17511650bb12f/src/lib/ui/templates/SeaweedLayout/SeaweedLayout.svelte#L248</TextLink>
+</p>
