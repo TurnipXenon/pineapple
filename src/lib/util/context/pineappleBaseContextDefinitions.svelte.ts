707a877cf7175bd7f088da662c30b94ef92c44f8
turnipxenon <turnipxenon@gmail.com>
2026-02-03 00:30:23 -0500

    Revert "fix: debug by removing all set Context API calls"
    This reverts commit 7d557302

--- src/lib/util/context/pineappleBaseContextDefinitions.svelte.ts
+++ src/lib/util/context/pineappleBaseContextDefinitions.svelte.ts
@@ -1,11 +1,13 @@
-import { createLocalStore } from "$pkg/util/localStore.svelte";
+import type { OverlayType, PageMeta } from "$pkg";
+import { LocalStore } from "$pkg/util/localStore.svelte";
+import { createContext } from "svelte";
 
 export const [getEnablePortraitContext, setEnablePortraitContext] = createContext<LocalStore<boolean>>();
 export const [getEnableDialogPreferenceContext, setEnableDialogPreferenceContext] = createContext<LocalStore<boolean>>();
 
 // note to self: proper two way binding is seen with the variables below
 // see https://svelte.dev/docs/svelte/context#Using-context-with-state
-export const [getEnableDialogOverlayContext, setEnableDialogOverlayContext] = createContext<{value: boolean}>();
+export const [getEnableDialogOverlayContext, setEnableDialogOverlayContext] = createContext<{ value: boolean }>();
 
 export const [getOverlayTypeContext, setOverlayTypeContext] = createContext<LocalStore<OverlayType>>();
 export const [getIgnoreOverlayOverride, setIgnoreOverlayOverride] = createContext<boolean>();
