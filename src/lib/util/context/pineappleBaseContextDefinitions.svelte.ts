fa3deda724c9cda278a48845a20ce69342f4be11
turnipxenon <turnipxenon@gmail.com>
2026-02-03 22:29:15 -0500

    Revert "fix: move from createContext to getContext/setContext pair"
    This reverts commit 80ed97c1ba264dc3619333f7e2cf14bd29fc047a.

--- src/lib/util/context/pineappleBaseContextDefinitions.svelte.ts
+++ src/lib/util/context/pineappleBaseContextDefinitions.svelte.ts
@@ -1,37 +1,14 @@
 import type { OverlayType, PageMeta } from "$pkg";
 import type { LocalStore } from "$pkg/util/localStore.svelte";
-import { getContext, setContext } from "svelte";
+import { createContext } from "svelte";
 
-const enablePortraitKey = Symbol("enable-portrait");
-const enableDialogPreferenceKey = Symbol("enable-dialog-preference");
-const enableDialogOverlayKey = Symbol("enable-dialog-overlay");
-const overlayTypeKey = Symbol("overlay-type");
-const ignoreOverlayOverrideKey = Symbol("ignore-overlay-override");
-const siteLayoutKey = Symbol("site-layout");
-
-export const getEnablePortraitContext = () => getContext<LocalStore<boolean>>(enablePortraitKey);
-export const setEnablePortraitContext = (value: LocalStore<boolean>) =>
-	setContext(enablePortraitKey, value);
-
-export const getEnableDialogPreferenceContext = () =>
-	getContext<LocalStore<boolean>>(enableDialogPreferenceKey);
-export const setEnableDialogPreferenceContext = (value: LocalStore<boolean>) =>
-	setContext(enableDialogPreferenceKey, value);
+export const [getEnablePortraitContext, setEnablePortraitContext] = createContext<LocalStore<boolean>>();
+export const [getEnableDialogPreferenceContext, setEnableDialogPreferenceContext] = createContext<LocalStore<boolean>>();
 
 // note to self: proper two way binding is seen with the variables below
 // see https://svelte.dev/docs/svelte/context#Using-context-with-state
-export const getEnableDialogOverlayContext = () =>
-	getContext<{ value: boolean }>(enableDialogOverlayKey);
-export const setEnableDialogOverlayContext = (value: { value: boolean }) =>
-	setContext(enableDialogOverlayKey, value);
+export const [getEnableDialogOverlayContext, setEnableDialogOverlayContext] = createContext<{value: boolean}>();
 
-export const getOverlayTypeContext = () => getContext<LocalStore<OverlayType>>(overlayTypeKey);
-export const setOverlayTypeContext = (value: LocalStore<OverlayType>) =>
-	setContext(overlayTypeKey, value);
-
-export const getIgnoreOverlayOverride = () => getContext<boolean>(ignoreOverlayOverrideKey);
-export const setIgnoreOverlayOverride = (value: boolean) =>
-	setContext(ignoreOverlayOverrideKey, value);
-
-export const getSiteLayout = () => getContext<PageMeta[]>(siteLayoutKey);
-export const setSiteLayout = (value: PageMeta[]) => setContext(siteLayoutKey, value);
+export const [getOverlayTypeContext, setOverlayTypeContext] = createContext<LocalStore<OverlayType>>();
+export const [getIgnoreOverlayOverride, setIgnoreOverlayOverride] = createContext<boolean>();
+export const [getSiteLayout, setSiteLayout] = createContext<PageMeta[]>();
