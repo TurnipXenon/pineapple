f0e203fe4f7a2d54773639fc04244185e4dd4194
turnipxenon <turnipxenon@gmail.com>
2026-02-21 02:17:42 -0500

    build: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,11 @@ export default defineConfig({
 			additionalFiles: { ".npmignore": "\n" }
 		})
 	],
-	assetsInclude: ["**/*.yarn"]
+	assetsInclude: ["**/*.yarn"],
+	// Tell Vitest to use the `browser` entry points in `package.json` files, even though it's running in Node
+	resolve: process.env.VITEST
+		? {
+			conditions: ["browser"]
+		}
+		: undefined
 });
