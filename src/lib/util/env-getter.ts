d1529a7eac34039edd2d77e94e8343e60b1f0e27
turnip@bok-choy <turnipxenon@gmail.com>
2026-09-21 03:13:58 -0400

    feat: move from gitlab to cloudflare r2

--- src/lib/util/env-getter.ts
+++ src/lib/util/env-getter.ts
@@ -5,6 +5,6 @@ const nodeEnv = typeof process !== "undefined" ? process.env : undefined;
 export const getCmsBaseUrl = () =>
 	import.meta.env.VITE_CMS_BASE_URL ??
 	nodeEnv?.VITE_CMS_BASE_URL ??
-	"https://gitlab.com/turnipxenon-personal/test-obdisian/-/raw/main";
+	"https://cms.pineapple.turnipxenon.com";
 export const getWebBaseUrl = () =>
 	import.meta.env.VITE_WEB_BASE_URL ?? nodeEnv?.VITE_WEB_BASE_URL ?? "/pineapple";
